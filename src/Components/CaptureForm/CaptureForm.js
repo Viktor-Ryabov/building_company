533dc60540ca0de2480f02fdbd87f4df362dbe7e
Viktor Ryabov <iprva@ya.ru>
2022-11-29 11:26:00 +0300

    modified button

--- src/Components/CaptureForm/CaptureForm.js
+++ src/Components/CaptureForm/CaptureForm.js
@@ -3,13 +3,12 @@ import Styles from "../CaptureForm/CaptureForm.module.css";
 const CaptureForm = () => {
     return (
         <div className={Styles.form}>
-            <h2 className={Styles.subtitle}>Заполни форму и получи смету беслпатно</h2>
+            <h2 className={Styles.subtitle}>Заполни форму и получи смету бесплатно</h2>
             <input className={Styles.input} minlength="3" required placeholder="Виктор Рябов" type="text" />
             <input className={Styles.input} minlength="3" required placeholder="+7 (999) 999-9999" type="tel" />
             <input className={Styles.input} type="email" name="user_email" required placeholder="ho4y-dom@ya.ru" />
             <textarea className={Styles.input} name="message" cols="30" rows="4" placeholder="Введите сообщение" />
-            <input className={Styles.input} type="file" id="file" name="file" multiple />
-            <button>Получить смету</button>
+            <a href="#" className={Styles.button}>Получить смету<span className={Styles.flare}></span></a>
         </div>
     );
 };
