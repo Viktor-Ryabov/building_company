88f9d4cc7dc7c833cecc81e8399f3c4199e327b0
Viktor Ryabov <iprva@ya.ru>
2022-12-19 13:06:11 +0300

    added benefits

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from '../Header/Header.js';
+import Benefit from "../Benefit/Benefit.js"
 import TopMenu from '../TopMenu/TopMenu.js';
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
 
       <Header/>
 
+      <Benefit/>
+
       <TopMenu/>
+
 
      
     </div>
